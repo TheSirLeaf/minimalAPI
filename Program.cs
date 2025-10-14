4a53eda9c41f074a68d4942dec25f32a859c903d
TheSirLeaf <patrickorb3@gmail.com>
2025-10-14 07:38:28 -0300

    Simplifica if

--- Program.cs
+++ Program.cs
@@ -6,13 +6,9 @@ app.MapGet("/", () => "Hello World!");
 app.MapPost("/login", (LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
-    {
         return Results.Ok("Login com sucesso!");
-    }
     else
-    {
         return Results.Unauthorized();
-    }
 });
 
 app.Run();
